8f6b8c707064b8f3518eb05cf999e43f11bc90af
IvanAndrienko <andrvan2002@mail.ru>
2022-11-14 17:24:42 +1000

    14.11
    dfhgh

---
+++ lab5/app/src/main/java/com/example/lab5/RecievedMessageActivity.java
@@ -0,0 +1,22 @@
+package com.example.lab5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecievedMessageActivity extends AppCompatActivity {
+
+    private TextView textViewReceivedMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recieved_message);
+        textViewReceivedMsg = findViewById(R.id.textViewReceivedMsg);
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra("msg");
+        textViewReceivedMsg.setText(msg);
+    }
+}
